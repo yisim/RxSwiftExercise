55bda7b986946e8c63184e04b1f5863d71dbb839
yisim <1006414553@qq.com>
2021-08-30 18:21:12 +0800

    update

--- rxs-materials-editions-4.0/04-observables-in-practice/projects/starter/Combinestagram/MainViewController.swift
+++ rxs-materials-editions-4.0/04-observables-in-practice/projects/starter/Combinestagram/MainViewController.swift
@@ -63,7 +63,7 @@ class MainViewController: UIViewController {
       })
       .disposed(by: bag)
     
-    test4()
+    test5()
     
   }
   
@@ -1030,6 +1030,80 @@ extension MainViewController {
     }
     
     
+  }
+}
+
+extension MainViewController {
+
+  func test5() {
+    let contacts = [
+      "603-555-1212": "Florent",
+      "212-555-1212": "Shai",
+      "408-555-1212": "Marin",
+      "617-555-1212": "Scott"
+    ]
+
+    func phoneNumber(from inputs: [Int]) -> String {
+      var phone = inputs.map(String.init).joined()
+
+      phone.insert("-", at: phone.index(
+        phone.startIndex,
+        offsetBy: 3)
+      )
+
+      phone.insert("-", at: phone.index(
+        phone.startIndex,
+        offsetBy: 7)
+      )
+
+      return phone
+    }
+
+    example(of: "phone") {
+      let disposeBag = DisposeBag()
+
+      let input = PublishSubject<Int>()
+
+      input
+        .skipWhile {
+          $0 == 0
+        }
+        .filter {
+          $0 < 10
+        }
+        .take(10)
+        .toArray()
+        .subscribe {
+          let phone = phoneNumber(from: $0)
+          if let contact = contacts[phone] {
+            print("Dialing \(contact) (\(phone))...")
+          } else {
+            print("Contact not found")
+          }
+
+        } onError: { error in
+          print(error)
+        }
+        .disposed(by: disposeBag)
+
+      input.onNext(0)
+      input.onNext(603)
+
+      input.onNext(2)
+      input.onNext(1)
+
+      // Confirm that 7 results in "Contact not found",
+      // and then change to 2 and confirm that Shai is found
+      input.onNext(2)
+
+      "5551212".forEach {
+        if let number = (Int("\($0)")) {
+          input.onNext(number)
+        }
+      }
+
+      input.onNext(9)
+    }
   }
 }
 
